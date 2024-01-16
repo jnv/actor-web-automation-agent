9f6f5645a0a55a14fd4e1c12240308e6886a7085
Jan Vlnas <git.noreply@jan.vlnas.cz>
2024-01-17 00:32:14 +0100

    Expand html elements whitelist

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ export const UNIQUE_ID_ATTRIBUTE = 'gid';
 // WEB AUTOMATION
 // TODO
 export const WHITELIST_TAGS_WEB_AUTOMATION = [
-    'html', 'body', 'title',
+    'html', 'head', 'meta', 'title', 'link', 'body',
     'main', 'footer', 'header', 'nav', 'section', 'article',
     'div', 'p',
     'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
@@ -16,6 +16,10 @@ export const WHITELIST_ATTRIBUTES_WEB_AUTOMATION = [
     UNIQUE_ID_ATTRIBUTE,
     'href',
     'alt',
+    'name',
+    'content',
+    'value',
+    'rel'
 ];
 
 export const HTML_CURRENT_PAGE_PREFIX = 'HTML of current page:' as const;
